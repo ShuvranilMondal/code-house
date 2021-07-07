39c7d1743989890b82370a7ad4b57f4a7d0b34f4
Shuvranil <shuvranilmondal@gmail.com>
2021-07-07 11:54:54 +0530

    add

--- js-file/file.js
+++ js-file/file.js
@@ -6,3 +6,20 @@ let navbar=()=>{
 navBtn.addEventListener("click",()=>{
     navbar()
 })
+
+
+let img = document.getElementById('img');
+
+let bgImageChnage=()=>{
+    let imgList = ["./img/b3.jpg","./img/b4.jpg","./img/b1.jpg"];
+    let i = 0;
+    setInterval(() => {
+        if(i > imgList.length-1){
+            i = 0;
+        }
+        img.src = imgList[i];
+        i++
+    }, 3000);
+}
+
+bgImageChnage()
